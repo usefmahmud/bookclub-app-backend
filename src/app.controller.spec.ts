da86e134662f56056e706e132941e49937a90db8
usefmahmud <usef.mahmoud@outlook.com>
2025-08-21 04:13:23 +0300

    chore(swagger): configure Swagger UI with custom CDN and update server status endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "server is running!"', () => {
+      expect(appController.getServerStatus()).toBe('server is running!');
     });
   });
 });

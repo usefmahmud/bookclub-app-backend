92e6f2bed70f5b70cfc21b4399f12bfcc6d393db
usefmahmud <usef.mahmoud@outlook.com>
2025-08-21 19:24:54 +0300

    feat(auth): integrate AuthService and add Swagger documentation to login endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegistrationDto } from './dto/registration.dto';
+import { AuthService } from './auth.service';
+import { ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginResponseSchema } from './dto/login-response.schema';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Post('/login')
-  login(@Body() loginDto: LoginDto) {
-    return {
-      message: 'Login successful',
-    };
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'Login success',
+    type: LoginResponseSchema,
+  })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  async login(@Body() loginDto: LoginDto): Promise<LoginResponseSchema> {
+    return await this.authService.login(loginDto);
   }
 
   @Post('/register')
